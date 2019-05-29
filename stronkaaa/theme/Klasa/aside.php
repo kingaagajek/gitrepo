aa2132c74d3d1e62543daa465c939b8e120ff423
kingaagajek <kinga.gajek@lo1.sandomierz.pl>
2019-05-29 09:40:47 +0200

    stornka szablony

---
+++ stronkaaa/theme/Klasa/aside.php
@@ -0,0 +1,17 @@
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); } ?>
+            <aside class="rounded p-2 text-md-right my-1 my-md-4">
+               <h3>Zobacz również</h3>
+               <ol class="d-none d-md-block">
+                  <li>Pierwszy element listy</li>
+                  <li>Drugi element listy
+                     <ul>
+                        <li>podpunkt 1</li>
+                        <li>podpunkt 2</li>
+                     </ul>
+                  </li>
+               </ol>
+               <ol class="list-inline d-md-none">
+                  <li class="list-inline-item">Pierwszy element listy</li>
+                  <li class="list-inline-item">Drugi element listy
+               </ol>
+            </aside>
