2404bea700d3b946b0f0a434790cef66406d02ff
Kinga Gajek <kinga.gajek@lo1.sandomierz.pl>
2019-09-21 22:50:50 +0200

    plan, trojkat w c++

--- cpp/program1.cpp
+++ cpp/program1.cpp
@@ -11,9 +11,9 @@ int main(int argc, char **argv)
 {
 	int liczba1;
     float liczba2;
-    cout << "Podaj liczbę całkowitą: ";
+    cout << "Podaj liczbe calkowita: ";
     cin >> liczba1;
-    cout << "Podałeś: " << liczba1;
+    cout << "Podales: " << liczba1;
 	return 0;
 
 }
--- cpp/trojkat.cpp
+++ cpp/trojkat.cpp
@@ -1,13 +1,12 @@
 /*
  * trojkat.cpp
- * 
+ *
  * Napisz program, który pobiera od użytkownika trzy liczby i sprawdza, czy da się z nich zbudować trójkąt.
  * Program powinien wyprowadzić odpowiedni komunikat.
  * Jeśli da się zbudować trójkąt, sprawdź, czy to trójkat prostokątny.
  * Wyprowadź odpowiedni komunikat.
- * 
+ *
  */
-
 
 #include <iostream>
 #include <cmath>
@@ -25,10 +24,10 @@ int main(int argc, char **argv)
     float bok3;
     cout << "Podaj trzeci bok: ";
     cin >> bok3;
-    if (bok1 + bok2 > bok3) cout << "To jest trójkąt";
-        else cout << "To nie jest trójkąt";
-            if ()
-
+    if ((bok1 + bok2 > bok3) && (bok1 + bok3 > bok2) && (bok2 + bok3 > bok1)) cout << "To jest trojkat." << endl;
+        else cout << "To nie jest trojkat." << endl;
+    if ((bok1 * bok1 + bok2 * bok2 ==  bok3 * bok3) || (bok1 * bok1 + bok3 * bok3 ==  bok2 * bok2) || (bok2 * bok2 + bok3 * bok3 ==  bok1 * bok1)) cout << "To jest trojkat prostokatny.";
+        else cout << "To nie jest trojkat prostokatny.";
 	return 0;
 }
 
