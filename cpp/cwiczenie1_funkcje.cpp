d784ad3b9360b86d65fe8cbf2e1596f8a26c3523
kingaagajek <kinga.gajek@lo1.sandomierz.pl>
2019-10-07 10:36:59 +0200

    funkcje i zmienne

---
+++ cpp/cwiczenie1_funkcje.cpp
@@ -0,0 +1,28 @@
+/*
+ * cwiczenie1_funkcje.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int staz = 1;
+float zarobek = 1000;
+
+void drukuj() {
+    cout << "Pracujesz" << staz << "lat i zarabiasz" << zarobek << endl;
+}
+
+void awans(int &staz, float &zarobek) {
+    staz = staz + 1;
+    zarobek = zarobek * 1,1;
+}
+
+int main(int argc, char **argv)
+{
+	awans(staz, zarobek);
+    drukuj();
+	return 0;
+}
+
