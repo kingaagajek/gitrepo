7b93d32c7501fcbe09c8fbf95276ae8003f898fc
Kinga Gajek <kinga.gajek@lo1.sandomierz.pl>
2019-10-09 21:07:32 +0200

    ćwiczenie funkcje

--- cpp/cwiczenie1_funkcje.cpp
+++ cpp/cwiczenie1_funkcje.cpp
@@ -11,16 +11,19 @@ int staz = 1;
 float zarobek = 1000;
 
 void drukuj() {
-    cout << "Pracujesz" << staz << "lat i zarabiasz" << zarobek << endl;
+    cout << "Pracujesz " << staz << " lat i zarabiasz " << zarobek << endl;
 }
 
 void awans(int &staz, float &zarobek) {
     staz = staz + 1;
-    zarobek = zarobek * 1,1;
+    zarobek = zarobek * 1.1;
 }
 
 int main(int argc, char **argv)
 {
+    int lata;
+    cout << "Podaj staz pracy w latach: " << endl;
+    cin >> lata;
 	awans(staz, zarobek);
     drukuj();
 	return 0;
