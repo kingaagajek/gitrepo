27bbe6b218f5a0e7e1381a1dc97e976c3bdf779c
kingaagajek <kinga.gajek@lo1.sandomierz.pl>
2019-10-17 11:40:27 +0200

    tablica, srednia ocen

--- cpp/tablica.cpp
+++ cpp/tablica.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 	int rozmiar = 5;
 	int tablica[rozmiar];
 	int i = 0;
-	cout << "Podaj 20 liczb" << endl;
+	cout << "Podaj 5 liczb:" << endl;
 	for (i=0; i<rozmiar; i++) {
 		cin >> tablica[i];
 	}
