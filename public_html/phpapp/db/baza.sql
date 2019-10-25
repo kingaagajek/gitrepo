06c5311d8473ed20f0625ddc490f43e26f145791
kingaagajek <kinga.gajek@lo1.sandomierz.pl>
2019-10-25 09:42:08 +0200

    dalej php, tablice php

--- public_html/phpapp/db/baza.sql
+++ public_html/phpapp/db/baza.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS menu;
 CREATE TABLE menu (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	plik CHAR(20) NOT NULL,
@@ -9,4 +10,5 @@ INSERT INTO menu VALUES(NULL, 'witam', 'Witamy', 1);
 INSERT INTO menu VALUES(NULL, 'formularz', 'Formularz', 2);
 INSERT INTO menu VALUES(NULL, 'klasa', 'Klasa', 3);
 
--- INSERT INTO menu(tytul, plik, id) VALUES('')
+-- INSERT INTO menu(tytul, plik, id) VALUES('Klasa', 'klasa', NULL);
+-- sqlite3 baza.db < baza.sql
