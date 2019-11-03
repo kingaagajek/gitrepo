c0256a92d15db36cf2cdd317cf6c1507dd022716
Kinga Gajek <kinga.gajek@lo1.sandomierz.pl>
2019-11-03 21:43:12 +0100

    Program liczący ilość słów

---
+++ cpp/liczwyrazy.cpp
@@ -0,0 +1,43 @@
+/*
+ * tekst.cpp
+ */
+
+
+#include <iostream>
+
+#define ROZMIAR 100
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	char tekst[ROZMIAR];
+	int i;
+	int slowa = 1;
+	cout << "Podaj zdanie:" << endl;
+	// cin >> tekst;
+	cin.getline(tekst, ROZMIAR);
+	// cout << tekst;
+	//for (i=0; i < ROZMIAR; i++) {
+	//	cout << tekst[i];
+	//}
+	//cout << endl;
+
+	i = 0;
+	while (tekst[i] != '\0') {
+		cout << tekst[i];
+		i++;
+
+	}
+	cout << endl;
+
+	for (int i=0; i < ROZMIAR ; i++) {
+        if (' '== tekst[i]) {
+            slowa++;
+        }
+	}
+	cout << "To zdanie ma " << slowa << " slow";
+
+	return 0;
+}
+
