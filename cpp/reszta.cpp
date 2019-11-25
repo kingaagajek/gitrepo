bf191516edc52534baf449b018c76078674ac97c
kingaagajek <kinga.gajek@lo1.sandomierz.pl>
2019-11-25 10:36:07 +0100

    schemat hornera

---
+++ cpp/reszta.cpp
@@ -0,0 +1,21 @@
+/*
+ * reszta.cpp
+ */
+
+
+#include <iostream>
+using  namespace std;
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
+R = 69;
+69 % 50 = 1
+1) R: 60 - 50 * (69 % 50) = 19
+2) R: 19 - 10 * (19 % 10) = 9
+3) R: 9 - 5 * (9 % 5) = 4
+4) R: 4 - 2 * (4 % 2) = 0
+
